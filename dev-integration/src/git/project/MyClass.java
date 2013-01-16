6b2b0bd77d895f3ea18ed15525eb645f50054995
leonardo.borysiuk <leonardo.borysiuk@MW7CD4COFQXX54.dir.svc.accenture.com>
2013-01-16 15:34:36 -0300

    U Like It

--- dev-integration/src/git/project/MyClass.java
+++ dev-integration/src/git/project/MyClass.java
@@ -2,9 +2,7 @@ package git.project;
 
 public class MyClass {
 	public static void main(String[] args) {
-	    System.out.println("Git is so funny");
-	    int contador = 20;
-	    
+	    System.out.println("Git is so funny");    
 	  }
 
 }
